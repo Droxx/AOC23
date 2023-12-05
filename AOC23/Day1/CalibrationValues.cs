7ca1435822787b88f9df40082edc0d5849078cc5
Michael Stocks <michaelstocks1@gmail.com>
2023-12-05 09:49:07 +0100

    day 1 solution

---
+++ AOC23/Day1/CalibrationValues.cs
@@ -0,0 +1,103 @@
+﻿namespace AOC23.Day1;
+
+public class CalibrationValues
+{
+    private readonly string _input;
+    
+    public CalibrationValues(string input)
+    {
+        _input = input;
+    }
+
+    public int Compute()
+    {
+        var lines = _input.Split("\n");
+        var total = 0;
+
+        foreach (var line in lines.Where(l => l.Length > 0))
+        {
+            total += GetTotal(line);
+        }
+
+        return total;
+    }
+
+    private int GetTotal(string line)
+    {
+        int? first = null;
+        int? last = null;
+        for (var i = 0; i < line.Length; i++)
+        {
+            var sub = line.Substring(i);
+            var digit = GetStrDigit(sub);
+            if (digit != null)
+            {
+                if (first == null)
+                {
+                    first = digit.Item1;
+                }
+                else
+                {
+                    last = digit.Item1;
+                }
+            }
+            else
+            {
+                if (int.TryParse(line[i].ToString(), out var dig))
+                {
+                    if (first == null)
+                    {
+                        first = dig;
+                    }
+                    else
+                    {
+                        last = dig;
+                    }
+                }
+            }
+        }
+        return int.Parse($"{first}{last ?? first}");
+    }
+
+    private Tuple<int, int>? GetStrDigit(string sub)
+    {
+        if (sub.StartsWith("one"))
+        {
+            return new Tuple<int, int>(1,3);
+        }
+        else if (sub.StartsWith("two"))
+        {
+            return new Tuple<int, int>(2,3);
+        }
+        else if (sub.StartsWith("three"))
+        {
+            return new Tuple<int, int>(3,5);
+        }
+        else if (sub.StartsWith("four"))
+        {
+            return new Tuple<int, int>(4,4);
+        }
+        else if (sub.StartsWith("five"))
+        {
+            return new Tuple<int, int>(5,4);
+        }
+        else if (sub.StartsWith("six"))
+        {
+            return new Tuple<int, int>(6,3);
+        }
+        else if (sub.StartsWith("seven"))
+        {
+            return     new Tuple<int, int>(7,5);
+        }
+        else if (sub.StartsWith("eight"))
+        {
+            return new Tuple<int, int>(8,5);
+        }
+        else if (sub.StartsWith("nine"))
+        {
+            return new Tuple<int, int>(9,4);
+        }
+
+        return null;
+    }
+}
--- AOC23/Program.cs
+++ AOC23/Program.cs
@@ -1,3 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using AOC23.Day1;
+
+Console.WriteLine("AOC 23 - BEGIN");
+Console.WriteLine("Which day?");
+var dayStr = Console.ReadLine();
+
+if (int.TryParse(dayStr, out var day))
+{
+    switch (day)
+    {
+        case 1:
+            Console.WriteLine("Please enter calibration input, followed by an empty line to compute");
+            string calibInput = "";
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line == "")
+                {
+                    break;
+                }
+                calibInput += line + "\n";
+            }
+            var calibration = new CalibrationValues(calibInput);
+            Console.WriteLine(calibration.Compute());
+            break;
+        default:
+            Console.WriteLine("Day not implemented");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("Invalid day");
+}
+
+Console.WriteLine("Press any key to exit...");
