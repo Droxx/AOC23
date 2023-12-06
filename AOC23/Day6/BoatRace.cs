e9e330d4bcb1cd784157615054d420cf3477925c
Michael Stocks <michaelstocks1@gmail.com>
2023-12-06 17:13:51 +0100

    day 6 complete

--- AOC23/Day6/BoatRace.cs
+++ AOC23/Day6/BoatRace.cs
@@ -6,32 +6,38 @@ public class BoatRace
 {
     private List<Race> _races = new List<Race>();
     
-    public int WinOptionsMult(string input)
+    public long WinOptionsMult(string input)
     {
         ParseInput(input);
 
-        return 5;
+        long winMult = 0;
+        foreach (var race in _races)
+        {
+            winMult = winMult == 0 ? race.GetWinTimes() : race.GetWinTimes() * winMult;
+        }
+
+        return winMult;
     }
 
     private void ParseInput(string input)
     {
         var lines = input.Split("\n").Where(l => !string.IsNullOrEmpty(l)).ToList();
-        var times = new List<int>();
-        var distances = new List<int>();
+        var times = new List<long>();
+        var distances = new List<long>();
         var regex = new Regex(@"\.*(\d+)");
          
         // Times
         var timeMatches= regex.Matches(lines[0]);
         for(var i = 0; i < timeMatches.Count; i++)
         {
-            times.Add(int.Parse(timeMatches[i].Groups[1].Value));
+            times.Add(long.Parse(timeMatches[i].Groups[1].Value));
         }
         
         // Distances
         var distanceMatches= regex.Matches(lines[1]);
         for(var i = 0; i < distanceMatches.Count; i++)
         {
-            distances.Add(int.Parse(distanceMatches[i].Groups[1].Value));
+            distances.Add(long.Parse(distanceMatches[i].Groups[1].Value));
         }
 
         if (times.Count != distances.Count)
@@ -52,7 +58,26 @@ public class BoatRace
 
     private class Race
     {
-        public int Time { get; set; }
-        public int Distance { get; set; }
+        public long Time { get; set; }
+        public long Distance { get; set; }
+
+        public long GetWinTimes()
+        {
+            var winCounter = 0;
+            for(long i = 0; i <= Time; i++)
+            {
+                var speed = i;
+                var remainingTime = Time - i;
+
+                var totalDistance = speed * remainingTime;
+                
+                if(totalDistance > Distance)
+                {
+                    winCounter++;
+                }
+            }
+
+            return winCounter;
+        }
     }
 }
