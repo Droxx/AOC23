c7910c8ca28c98fd4fa56e95014c368230742f94
Michael Stocks <michaelstocks1@gmail.com>
2023-12-13 13:40:24 +0100

    working on an exception approeach

--- AOC23/Day13/Mirrors.cs
+++ AOC23/Day13/Mirrors.cs
@@ -18,9 +18,31 @@ public class Mirrors
     {
         var verRowsAbove = 0;
         var horColsLeft = 0;
+        var verInd = -1;
+        var horInd = -1;
+        var isClean = false;
 
-        var verInd = GetReflectionIndex(map.Cols);
-        var horInd = GetReflectionIndex(map.Rows);
+
+        try
+        {
+            verInd = GetReflectionIndex(map.Cols,isClean);
+        }
+        catch (SmudgeException ex)
+        {
+            isClean = true;
+            verInd = GetReflectionIndex(ex.RepairedLines, isClean);
+        }
+
+        try
+        {
+            horInd = GetReflectionIndex(map.Rows);
+        }
+        catch (SmudgeException ex)
+        {
+            isClean = true;
+            horInd = GetReflectionIndex(ex.RepairedLines, isClean);
+        }
+
 
         //verRowsLeft = Math.Abs(map.Cols.Count - (map.Cols.Count - verInd));
         //horColsAbove = Math.Abs(map.Rows.Count - (map.Rows.Count -horInd));
@@ -53,13 +75,23 @@ public class Mirrors
         return horColsLeft;
     }
 
-    private int GetReflectionIndex(List<string> list)
+    private int GetReflectionIndex(List<string> list, bool isClean = false)
     {
         var currentReflectionSize = 0;
         var reflectionStartAfterIndex = -1;
         for(int i=0;i<list.Count-1;i++)
         {
-            var currentReflection = LengthOfReflection(list, i);
+            int currentReflection = -1;
+            try
+            {
+                currentReflection = LengthOfReflection(list, i, isClean);
+            }
+            catch (RepairedLineException ex)
+            {
+                list[i] = ex.RepairedA;
+                throw new SmudgeException(list);
+            }
+
             if(currentReflection > currentReflectionSize)
             {
                 currentReflectionSize = currentReflection;
@@ -70,16 +102,21 @@ public class Mirrors
         return reflectionStartAfterIndex;
     }
 
-    private int LengthOfReflection(List<string> list, int startAfterIndex)
+    private int LengthOfReflection(List<string> list, int startAfterIndex, bool isClean = false)
     {
         var a = startAfterIndex;
         var b = startAfterIndex + 1;
-        while (a >= 0 && b < list.Count) 
+        while (a >= 0 && b < list.Count)
         {
-            if (list[a] == list[b])
+            var compared = CompareLines(list[a], list[b], out var repairedA);
+            if (compared == 0)
             {
                 a--;
                 b++;
+            }
+            else if (compared == 1 && !isClean)
+            {
+                throw new RepairedLineException(repairedA);
             }
             else
             {
@@ -119,6 +156,48 @@ public class Mirrors
                 map.Cols.Add(col);
             }
             _maps.Add(map);
+        }
+    }
+
+    private int CompareLines(string a, string b, out string? repairedA)
+    {
+        var repairedAArr = a.ToCharArray();
+        repairedA = null;
+        if (a == b)
+        {
+            return 0;
+        }
+
+        var diffs = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] != b[i])
+                diffs++;
+            if(diffs == 1)
+            {
+                repairedAArr[i] = b[i];
+            }
+        }
+
+        repairedA = new string(repairedAArr);
+        return diffs;
+    }
+
+    private class RepairedLineException : Exception
+    {
+        public string RepairedA { get; set; }
+        public RepairedLineException(string repairedLine) : base("REPAIRED")
+        {
+            RepairedA = repairedLine;
+        }
+    }
+
+    private class SmudgeException : Exception
+    {
+        public List<string> RepairedLines { get; set; }
+        public SmudgeException(List<string> repairedLines) : base("REPAIRED")
+        {
+            RepairedLines = repairedLines;
         }
     }
 
