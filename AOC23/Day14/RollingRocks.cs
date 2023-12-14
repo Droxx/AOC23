4dfaf26a55f259d0fc32528c7334d4d0d4d348c4
Michael Stocks <michaelstocks1@gmail.com>
2023-12-14 11:54:54 +0100

    Added rotation but my transposing is bad

--- AOC23/Day14/RollingRocks.cs
+++ AOC23/Day14/RollingRocks.cs
@@ -3,22 +3,51 @@
 public class RollingRocks
 {
     private readonly Grid _grid = new Grid();
-    
+    private const int CYCLES = 1;
     public long Calculate(string input)
     {
         ParseInput(input);
-        RollRocksNorth();
+        for(int i =0;i < CYCLES; i++)
+        {
+            Cycle();
+        }
         long result = SumLoad();
         return result;
     }
 
+    private void Cycle()
+    {
+        // North
+        _grid.Transpose();
+        _grid.PrintGrid();
+        RollRocksNorth();
+        _grid.PrintGrid();
+        
+        // West
+        _grid.Transpose();
+        _grid.PrintGrid();
+        RollRocksNorth();
+        _grid.PrintGrid();
+        
+        // South
+        _grid.Transpose();
+        _grid.PrintGrid();
+        RollRocksNorth();
+        _grid.PrintGrid();
+        
+        // East
+        _grid.Transpose();
+        _grid.PrintGrid();
+        RollRocksNorth();
+        _grid.PrintGrid();
+    }
+
     private long SumLoad()
     {
-        
+        _grid.PrintGrid();;
         long total = 0;
-        foreach (var col in _grid.Cols)
+        foreach (var col in _grid.Rows)
         {
-            Console.WriteLine(col);
             for (int i = 0; i < col.Length; i++)
             {
                 if (col[i] == 'O')
@@ -33,12 +62,12 @@ public class RollingRocks
     
     private void RollRocksNorth()
     {
-        for (int x = 0; x < _grid.Cols.Count; x++)
+        for (int x = 0; x < _grid.Rows.Count; x++)
         {
             var rollingBoulders = 0;
-            for (int i = _grid.Cols[x].Length -1; i >= 0 ; i--)
+            for (int i = _grid.Rows[x].Length -1; i >= 0 ; i--)
             {
-                var col = _grid.Cols[x];
+                var col = _grid.Rows[x];
 
                 // If rolling boulder, remove and add to boulder list
                 if (col[i] == 'O')
@@ -46,7 +75,7 @@ public class RollingRocks
                     rollingBoulders++;
                     var lineArr = col.ToCharArray();
                     lineArr[i] = '.';
-                    _grid.Cols[x] = new string(lineArr);
+                    _grid.Rows[x] = new string(lineArr);
                 }
                 // If rock boulder, stack pending boulders behind it
                 if(col[i] == '#')
@@ -56,7 +85,7 @@ public class RollingRocks
                     {
                         lineArr[n] = 'O';
                     }
-                    _grid.Cols[x] = new string(lineArr);
+                    _grid.Rows[x] = new string(lineArr);
                     rollingBoulders = 0;
                 }
                 else if (i == 0) // Otherwise, check if we're at the end
@@ -66,7 +95,7 @@ public class RollingRocks
                     {
                         lineArr[n] = 'O';
                     }
-                    _grid.Cols[x] = new string(lineArr);
+                    _grid.Rows[x] = new string(lineArr);
                     continue;
                 }
             }
@@ -77,23 +106,33 @@ public class RollingRocks
     {
         var lines = input.Split('\n').Where(l => !string.IsNullOrWhiteSpace(l)).ToList();
         _grid.Rows = lines;
-
-        var cols = new string[lines.Count];
-
-        for (var x = 0; x < lines[0].Length; x++)
-        {
-            for (var y = 0; y < lines.Count; y++)
-            {
-                cols[x] += lines[y][x];
-            }
-        }
-
-        _grid.Cols = cols.ToList();
     }
 
     private class Grid
     {
         public List<string> Rows { get; set; } = new List<string>();
-        public List<string> Cols { get; set; } = new List<string>();
+
+        public void PrintGrid()
+        {
+            foreach(var row in Rows)
+            {
+                Console.WriteLine(row);
+            }
+            Console.WriteLine();
+        }
+        
+        public void Transpose()
+        {
+            var newList = new string[Rows.Count];
+            for (var x = 0; x < Rows[0].Length; x++)
+            {
+                for (var y = 0; y < Rows.Count; y++)
+                {
+                    newList[x] += Rows[y][x];
+                }
+            }
+
+            Rows = newList.ToList();
+        }
     }
 }
