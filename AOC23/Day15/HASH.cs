dabeb92fad0d9f08bbdf8f6f9c25595707505fae
michael <michaelstocks1@gmail.com>
2023-12-15 12:59:15 +0100

    day 15 part 2

--- AOC23/Day15/HASH.cs
+++ AOC23/Day15/HASH.cs
@@ -1,4 +1,5 @@
 using System.IO.Pipes;
+using System.Text.RegularExpressions;
 
 namespace AOC23.Day15;
 
@@ -25,31 +26,72 @@ public class HASH
         ParseInput(input);
         foreach (var instructon in _instructions)
         {
-            _resultSum += HashInstruction(instructon);
+            ProcessInstruction(instructon);
         }
 
-        return _resultSum;
+        return CalculateFocusingPower();
     }
 
     private void ProcessInstruction(string instruction)
     {
         // For each instruction
-        
+
         // First string is the LABEL of the lens on which the step operates
         // The result of hashing that LABEL is the box fot that step
+        var regex = new Regex(@"([a-z]+)[=|-|,]*");
+        var matches = regex.Matches(instruction);
+        var label = matches[0].Groups[1].Value;
         
-        // Then = or -
-        
-        // ON -
-        // Go to the box and remove the lens with the LABEL
-        // Move any other lenses in that box as far forward as they can go (without changin order)
-        
-        // ON = (will be followed by a number)
-        // Numebr is focal length
-        // We may only see this once, so make sure to associate that label with the focal length for future use
-        // IF box contains a lens with that LABEL, remove the old lens, and replace with the new focal length
-        // IF the box does not contain a lens with that LABEL, then add that lens BEHIND all other lenses in box, don't move any other lenses
-        
+        var hash = Hash(label);
+        var box = _boxes[hash];
+
+        var remaining = instruction.Replace(label, "");
+        if (!remaining.Any())
+        {
+            // NO INSTRUCTION AFTER, LOOK UP IN LENS TABLE
+            throw new Exception("Should this happen?");
+            // Looking over the data it seems this may never be the case
+        }
+        else if (remaining[0] == '-')
+        {
+            // ON -
+            // Go to the box and remove the lens with the LABEL
+            // Move any other lenses in that box as far forward as they can go (without changin order)
+            if (box.Lenses.TryGetValue(label, out var existingLens))
+            {
+                var slotNoOfLens = existingLens.SlotNumber;
+                box.Lenses.Remove(label);
+                foreach (var subsequentLens in box.Lenses.Values.Where(l => l.SlotNumber > slotNoOfLens))
+                {
+                    subsequentLens.SlotNumber--;
+                }
+            }
+        }
+        else if(remaining[0] == '=')
+        {
+            // ON = (will be followed by a number)
+            // Number is focal length
+
+            var focalLength = int.Parse(remaining.Substring(1));
+
+            if (box.Lenses.TryGetValue(label, out var existingLens))
+            {
+                // IF box contains a lens with that LABEL, remove the old lens, and replace with the new focal length
+                existingLens.FocalLength = focalLength;
+            }
+            else
+            {
+                // IF the box does not contain a lens with that LABEL, then add that lens BEHIND all other lenses in box, don't move any other lenses
+                var currentHighSlotNo = box.Lenses.Any() ? box.Lenses.Max(l => l.Value.SlotNumber) : 0;
+                box.Lenses.Add(label, new Lens
+                {
+                    FocalLength = focalLength,
+                    SlotNumber = currentHighSlotNo + 1
+                });
+            }
+            
+            // We may only see this once, so make sure to associate that label with the focal length for future use
+        }
     }
 
     private long CalculateFocusingPower()
@@ -58,9 +100,9 @@ public class HASH
         {
             var boxInt = 1 + box.Key;
             var runningValueOfBox = 0;
-            for (int s = 0; s < box.Value.Lenses.Count; s++)
+            foreach (var lens in box.Value.Lenses)
             {
-                var lensVal = boxInt * (1 + s) * box.Value.Lenses[s].FocalLength;
+                var lensVal = boxInt * lens.Value.SlotNumber * lens.Value.FocalLength;
                 runningValueOfBox += lensVal;
             }
 
@@ -71,7 +113,7 @@ public class HASH
     }
     
 
-    private long HashInstruction(string instruction)
+    private byte Hash(string instruction)
     {
         var currentValue = 0;
         foreach (var ch in instruction.TrimEnd())
@@ -82,7 +124,7 @@ public class HASH
             currentValue %= 256;
         }
 
-        return currentValue;
+        return (byte)currentValue;
     }
 
     private void ParseInput(string input)
@@ -92,17 +134,15 @@ public class HASH
 
     private class Box
     {
-        public List<Lens> Lenses { get; set; } = new();
-        
+        public Dictionary<string, Lens> Lenses { get; set; } = new();
         public long FocusingPower { get; set; }
 
     }
 
     private class Lens
     {
-        public string LensCode { get; set; }
+        public int SlotNumber { get; set; }
         public int FocalLength { get; set; } // 1-9
-        public string Hash { get; set; }
         
     }
 }
